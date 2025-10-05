866909fef4d8de8b45575795a4be9e10b3ee60b3
April Nagel <april.n.nagel@gmail.com>
2025-10-05 12:13:34 -0500

    amended code to include receipt

--- question1.py
+++ question1.py
@@ -1,12 +1,27 @@
-print("Welcome to Bagel Theatre! To purchase a ticket, please enter your age below")
+print("Welcome to Bagel Theatre! To purchase a ticket, please enter your info below:")
 
-age = int(input("Enter your age: "))
 
-if age <= 13:
-    print("Child ticket (under 13): $8")
-elif age >= 13 and age < 64:
-    print("Adult ticket (13-64): $12 ")
-elif age >= 65:
-    print("Senior ticket (+65): $9")
-else:
-    print("Please enter a valid age")
+    
+name = input("Enter your name: ")
+age = int(input("Enter your age: ")) 
+ 
+try: 
+    if age <= 13:
+        price_per_ticket = 8 
+    elif age >= 13 and age < 64:
+        price_per_ticket = 12
+    elif age >= 65:
+         price_per_ticket = 9
+except ValueError:
+    print("Please enter valid age")
+
+quantity = int(input("How many tickets do you want? "))
+    
+total = (age * quantity)
+
+print("🎟️ Movie Ticket Receipt 🎟️")
+print(f"Customer name:  {name}")
+print(f"Ticket type:    {"Child ($8.00)" if age <= 13 else "Adult ($12.00)" if age >= 13 and age < 64 else "Senior ($9.00)"}")
+print(f"Quantity:       {quantity} ")    
+print(f"Total:          ${total}")
+print("Thank you for your purchase! Enjoy your movie!")
